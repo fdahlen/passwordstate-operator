e7a379271526b8376ea68fa2cc122c36e5654cfd
Fredrik Dahlén <fredrik.dahlen@gmail.com>
2021-08-18 23:16:32 +0200

    Fix deploy script for simulator

--- PasswordstateSimulator/deploy/deploy.ps1
+++ PasswordstateSimulator/deploy/deploy.ps1
@@ -5,7 +5,7 @@ $url = "http://passwordstatesimulator.passwordstate-simulator.svc.cluster.local"
 #$image = "nginxdemos/hello:plain-text"
 
 (Get-Content deployment.yaml).
-    replace('%%image%%', $image) |
+    replace('%%image%%', $image).
     replace('%%url%%', $url) |
     kubectl apply -n $namespace -f -
 
