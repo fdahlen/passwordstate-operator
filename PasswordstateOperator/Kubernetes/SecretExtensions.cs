90be91e12ca16f052b4e14348c825b6ce891cd6c
Fredrik Dahlén <fredrik.dahlen@gmail.com>
2021-08-27 00:05:28 +0200

    Fix for V1Secret.DataEquals extension method

--- PasswordstateOperator/Kubernetes/SecretExtensions.cs
+++ PasswordstateOperator/Kubernetes/SecretExtensions.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
 using k8s.Models;
 
@@ -7,10 +13,22 @@ namespace PasswordstateOperator.Kubernetes
     {
         public static bool DataEquals(this V1Secret first, V1Secret second)
         {
-            var firstJson = JsonSerializer.Serialize(first.Data);
-            var secondJson = JsonSerializer.Serialize(second.Data);
+            return GetContent(first).Equals(GetContent(second));
+        }
+        
+        private static string GetContent(V1Secret secret)
+        {
+            if (secret.StringData != null)
+            {
+                return string.Join("|", secret.StringData.Select(kvp => $"{kvp.Key}:{kvp.Value}").OrderBy(s => s));
+            }
 
-            return firstJson.Equals(secondJson);
+            if (secret.Data != null)
+            {
+                return string.Join("|", secret.Data.Select(kvp => $"{kvp.Key}:{Encoding.UTF8.GetString(kvp.Value)}").OrderBy(s => s));
+            }
+
+            throw new ArgumentException("No data in secret");
         }
     }
 }
